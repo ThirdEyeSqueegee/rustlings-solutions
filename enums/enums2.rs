45dd1a2c8e10e5f754026da530f7163a2191e822
Angad <66992519+ThirdEyeSqueegee@users.noreply.github.com>
2024-01-09 11:19:58 -0800

    Initial commit

---
+++ enums/enums2.rs
@@ -0,0 +1,39 @@
+// enums2.rs
+//
+// Execute `rustlings hint enums2` or use the `hint` watch subcommand for a
+// hint.
+
+#[derive(Debug)]
+enum Message {
+    Move { x: u32, y: u32 },
+    Echo(String),
+    ChangeColor(u8, u8, u8),
+    Quit,
+}
+
+impl Message {
+    fn call(&self) {
+        println!("{:?}", self);
+    }
+
+    fn Echo(&self, src: String) -> String {
+        src
+    }
+
+    fn ChangeColor(&self, r: u8, g: u8, b: u8) -> (u8, u8, u8) {
+        (r, g, b)
+    }
+}
+
+fn main() {
+    let messages = [
+        Message::Move { x: 10, y: 30 },
+        Message::Echo(String::from("hello world")),
+        Message::ChangeColor(200, 255, 255),
+        Message::Quit,
+    ];
+
+    for message in &messages {
+        message.call();
+    }
+}
